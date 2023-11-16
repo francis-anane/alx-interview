a206cb8c4508374a6e331ad24d3bd0ea3c662cb0
Francis Ofori Anane <ofori.anane@gmail.com>
2023-11-16 20:47:37 +0000

    Modify file

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,6 +3,7 @@
 Rotate a 2D matrix 90° clockwise
 """
 
+
 def rotate_2d_matrix(matrix):
     """
     Rotates a 2D matrix 90° clockwise.
@@ -47,4 +48,3 @@ def rotate_2d_matrix(matrix):
 # matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 # rotate_2d_matrix(matrix)
 # print(matrix)
-
