eee7a993ff7ad2b26c23fb548db7fda5c894b391
Francis Ofori Anane <ofori.anane@gmail.com>
2023-11-27 00:54:40 +0000

    Modify file

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -14,16 +14,17 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    table = [sys.maxsize for i in range(total + 1)]
-    table[0] = 0
-    m = len(coins)
-    for i in range(1, total + 1):
-        for j in range(m):
-            if coins[j] <= i:
-                subres = table[i - coins[j]]
-                if subres != sys.maxsize and subres + 1 < table[i]:
-                    table[i] = subres + 1
 
-    if table[total] == sys.maxsize:
-        return -1
-    return table[total]
+    # Initialize an array to store the minimum number of coins needed for each value from 0 to total
+    dp = [float('inf')] * (total + 1)
+
+    # Base case: 0 coins needed to make change for 0
+    dp[0] = 0
+
+    # Update the dp array for each coin value
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    # Check if the total can be made with the available coins
+    return dp[total] if dp[total] != float('inf') else -1
