34a86f9b30e24536551f76873cb9b7b1c81d21c1
Francis Ofori Anane <ofori.anane@gmail.com>
2023-11-27 01:29:41 +0000

    Modify file

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -15,48 +15,20 @@ def makeChange(coins, total):
              If total is 0 or less, returns 0.
              If total cannot be met by any number of coins, returns -1.
     """
+    if total <= 0:
+        return 0
 
-    # Dictionary to store computed results for memoization
-    memo = {}
+    # Initialize an array to store the minimum number of
+    # coins needed for each value from 0 to total
+    dp = [float('inf')] * (total + 1)
 
-    def minCoins(target):
-        """
-        Helper function for recursive computation of minimum coins needed.
+    # Base case: 0 coins needed to make change for 0
+    dp[0] = 0
 
-        Args:
-            target (int): Current target amount.
+    # Update the dp array for each coin value
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
 
-        Returns:
-            int: Minimum number of coins needed to meet the target.
-        """
-
-        # Check if the result for the current target is already memoized
-        if target in memo:
-            return memo[target]
-
-        # Base cases
-        if target == 0:
-            return 0
-
-        if target < 0:
-            return float('inf')
-
-        # Initialize with a large value
-        min_coins = float('inf')
-
-        # Iterate over each coin value
-        for coin in coins:
-            # Recursive call to calculate the result for the current coin
-            result = 1 + minCoins(target - coin)
-            min_coins = min(min_coins, result)
-
-        # Memoize the result for the current target
-        memo[target] = min_coins
-
-        return min_coins
-
-    # Call the helper function to get the final result
-    result = minCoins(total)
-
-    # Return the result, or -1 if total cannot be met
-    return result if result != float('inf') else -1
+    # Check if the total can be made with the available coins
+    return dp[total] if dp[total] != float('inf') else -1
